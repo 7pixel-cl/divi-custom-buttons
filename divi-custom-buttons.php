d263c6f126db7ee889841fa61602664eb822e556
Marco Alvarado <marco.alvarado@jove.com>
2023-04-05 00:11:06 -0400

    Create a new version

--- divi-custom-buttons.php
+++ divi-custom-buttons.php
@@ -1,33 +1,16 @@
 <?php
 /*
-Plugin Name: Divi Custom Buttons
-Plugin URI: https://example.com/divi-custom-buttons
-Description: Adds custom buttons to the Divi Visual Builder and triggers Mixpanel tracking events.
+Plugin Name: Divi Custom Mixpanel Button
+Description: A custom module for Divi that adds a Mixpanel tracking button.
 Version: 1.0
 Author: Your Name
-Author URI: https://example.com
-License: GPL2
 */
 
-if (!defined('ABSPATH')) {
-    exit;
-}
-
-class Divi_Custom_Buttons
-{
-    public function __construct()
-    {
-        add_action('et_builder_ready', array($this, 'enqueue_assets'));
+if ( ! function_exists( 'divi_custom_mixpanel_button_init' ) ) :
+    function divi_custom_mixpanel_button_init() {
+        if ( class_exists( 'ET_Builder_Module' ) ) {
+            require_once( plugin_dir_path( __FILE__ ) . 'includes/modules/MixpanelButton/MixpanelButton.php' );
+        }
     }
-
-    public function enqueue_assets()
-    {
-        wp_enqueue_script('mixpanel', '//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js', array(), null, true);
-        wp_enqueue_script('divi-custom-buttons', plugin_dir_url(__FILE__) . 'js/divi-custom-buttons.js', array('jquery', 'mixpanel'), '1.0', true);
-        wp_localize_script('divi-custom-buttons', 'DiviCustomButtons', array(
-            'ajaxurl' => admin_url('admin-ajax.php'),
-        ));
-    }
-}
-
-new Divi_Custom_Buttons();
+    add_action( 'divi_extensions_init', 'divi_custom_mixpanel_button_init' );
+endif;
